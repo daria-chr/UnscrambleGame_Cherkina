c36252af4911e62b895a1645efa2da48dc1ea009
daria-chr <cherkinadar@gmail.com>
2026-03-11 16:25:13 +0300

    Step 4: Add words data and game logic

---
+++ app/src/main/java/com/cherkina/unscramblegame/ui_model/GameScreen.kt
@@ -0,0 +1,44 @@
+package com.cherkina.unscramblegame.ui_model
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.cherkina.unscramblegame.data.GameViewModel
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun GameScreen(
+    modifier: Modifier= Modifier,
+    gameViewModel: GameViewModel= viewModel()
+){
+    val gameUiState by gameViewModel.uiState.collectAsState()
+    Column (
+        modifier= Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Text(
+            text = "Unscramble Game",
+            style = MaterialTheme.typography.headlineLarge,
+            fontSize = 32.sp
+        )
+
+    }
+}
+
--- app/src/main/java/com/cherkina/unscramblegame/ui_model/GameViewModel.kt
+++ app/src/main/java/com/cherkina/unscramblegame/ui_model/GameViewModel.kt
@@ -5,17 +5,40 @@ import com.cherkina.unscramblegame.data.GameUiState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import com.cherkina.unscramblegame.data.allWords
+
 
 class GameViewModel: ViewModel() {
-    private val _uiState= MutableStateFlow(GameUiState())
+    private val _uiState = MutableStateFlow(GameUiState())
     val uiState: StateFlow<GameUiState> = _uiState.asStateFlow()
+    private lateinit var currentWord: String
+    private var usedWords: MutableSet<String> = mutableSetOf()
 
-    init {
-        resetGame()
+    private fun shuffleGurrentWord(word: String): String {
+        val tempWord = word.toCharArray()
+        tempWord.shuffle()
+        while (String(tempWord) == word) {
+            tempWord.shuffle()
+        }
+        return String(tempWord)
     }
 
-    fun resetGame(){
+    private fun pickRandomWordAndShuffle(): String {
+        currentWord = allWords.random()
+        while (usedWords.contains(currentWord)) {
+            currentWord = allWords.random()
+        }
+        usedWords.add(currentWord)
+        return shuffleGurrentWord(currentWord)
 
     }
 
+
+    fun resetGame() {
+        usedWords.clear()
+        _uiState.value = GameUiState(
+            currentScrambleWord = pickRandomWordAndShuffle()
+        )
+
+    }
 }
